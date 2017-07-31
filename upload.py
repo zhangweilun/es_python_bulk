0b90cc300e771ff77e17848fc1010eff2daed946
willian <18702515157@163.com>
2017-07-31 10:36:00 +0800

    s

--- upload.py
+++ upload.py
@@ -1,18 +1,48 @@
-from elasticsearch import Elasticsearch,helpers
+from elasticsearch import Elasticsearch, helpers
+import csv
+import time
 es = Elasticsearch(hosts="192.168.1.114")
 j = 0
 actions = []
-while j <= 100000:
-    action = {
-        "_index": "tickets-index",
-        "_type": "tickets",
-        "_id": j,
-        "_source": {
-            "any":"data" + str(j),
-            "timestamp": datetime.now()
+with open('/Users/willian/Desktop/go_es.csv',newline='') as csvFile:
+    spamreader = csv.reader(csvFile)
+    for row in spamreader:
+        # print(row)
+        # print(', '.join(row))
+        if row[0] == 'id':
+            continue
+        strptime = time.strptime(row[11], "%Y-%m-%d")
+        time_stamp = int(time.mktime(strptime))
+        action = {
+            "_index": "tickets-index",
+            "_type": "tickets",
+            "_id": j,
+            "_source": {
+                "OrderId": row[0],
+                "MudiPort": row[13],
+                "Purchaser": row[5],
+                "PurchaserAddress": row[6],
+                "SupplierId": row[7],
+                "PurchaserId": row[4],
+                "OrderNo": row[1],
+                "OrderWeight": row[2],
+                "Supplier": row[8],
+                "ProDesc": row[10],
+                "OriginalCountry": row[14],
+                "SupplierAddress": row[9],
+                "FrankTime": time_stamp,
+                "OrderVolume": row[3],
+                "QiyunPort": row[12],
+                "TradeNumber": 0
             }
         }
-    actions.append(action)
-    j += 1
+        actions.append(action)
+        j += 1
+        if j >= 1000:
+            helpers.bulk(es, actions)
+            actions.clear()
 
-helpers.bulk(es, actions)
+
+
+
+
